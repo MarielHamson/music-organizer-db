01b40ab24ecdcbcfd9c1f8526dd51135caaf2684
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-23 14:44:07 -0700

    add artist and album objects and methods
    Co-authored-by: SPdowns <spdowns@protonmail.com>

--- MusicOrganizer/Controllers/AlbumsController.cs
+++ MusicOrganizer/Controllers/AlbumsController.cs
@@ -6,5 +6,6 @@ namespace MusicOrganizer.Controllers
 {
   public class AlbumsController : Controller
   {
+
   }
 }
--- MusicOrganizer/Models/Album.cs
+++ MusicOrganizer/Models/Album.cs
@@ -2,8 +2,26 @@ namespace MusicOrganizer.Models
 {
   public class Album
   {
-
-    // properties, methods, etc. will go here.
-
+    public string AlbumTitle { get; set; }
+    public int Id { get; }
+    private static List<Album> _instances = new List<Album> { };
+    public Album(string albumTitle)
+    {
+      AlbumTitle = albumTitle;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+    public static List<Album> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
   }
 }
